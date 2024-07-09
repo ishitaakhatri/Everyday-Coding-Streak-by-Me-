980febb92090f84e87566cc1b4fff2e7a3ee501c
ishitaakhatri <ishita4777@gmail.com>
2024-07-09 05:57:55 +0530

    day7

--- day5/object_literals.js
+++ day5/object_literals.js
@@ -11,3 +11,23 @@ let student={
         console.log("welcome to exam")
     }
 }
+//cloning an object 
+let a={value:10};
+let b={};
+
+//what if we don't assign(changing the value inside of a too)
+//b=a;
+
+b.value=20;
+
+console.log(a);
+console.log(b);
+
+//what if we assign
+
+Object.assign(b,a);
+
+b.value=30
+
+console.log(a);
+console.log(b);
