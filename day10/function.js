8d577fa990cfa498930076df2f82f9c238f985e1
ishitaakhatri <ishita4777@gmail.com>
2024-07-13 16:45:05 +0530

    day10

---
+++ day10/function.js
@@ -0,0 +1,28 @@
+//function declaration 
+function sayhi()
+ {
+    console.log('hi')
+ }
+
+sayhi();
+
+//another way 
+
+let greeting = function() {
+    console.log("hello,evryone..!")
+}
+
+greeting();
+
+//default parameters 
+
+writecode();
+
+function writecode(language = 'javascript'){
+    console.log(`write code in ${language}`);
+}
+
+writecode('c#');
+
+
+ 
---
+++ day10/hoisting.js
@@ -0,0 +1,7 @@
+add(4,4)
+
+function add(num1,num2)
+ {
+    console.log(num1+num2)
+ }
+//hoisting means javascript moves all the function defination above the function call so that you can define a function anywehre inside the code and it still does not create a mess 
