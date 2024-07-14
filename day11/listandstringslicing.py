8e6619b8326ffc29e810c7a06b3b0b05f127edcd
ishitaakhatri <ishita4777@gmail.com>
2024-07-14 21:39:26 +0530

    day11

---
+++ day11/listandstringslicing.py
@@ -0,0 +1,23 @@
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+print(numbers[0])
+print(numbers[8])
+print(numbers[-1])
+print(numbers[-8])
+
+#slicing in list 
+ 
+print(numbers[2:-2])
+print(numbers[3:8])
+print(numbers[-7:-2])
+print(numbers[-2:])
+print(numbers[:8])
+
+#list[start:end:step]
+
+print(numbers[1:10:4])
+print(numbers[-1:2:-2])
+
+#reversing a list 
+
+print(numbers[::-1])
