b296bb094802353bc3de896d126c07af6561930a
ishitaakhatri <ishita4777@gmail.com>
2024-07-25 01:19:40 +0530

    day20

--- day19/test_calc.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import calc 
-
-class testcalc(unittest.TestCase):
-    def test_add(self):
-        result = calc.add(10,5)
-        self.assertEqual(result , 15)
---
+++ day20/numpy_beggining/numpy_array.py
@@ -0,0 +1,4 @@
+import numpy as np
+my_list = [1,2,3,4,5,6,7]
+my_array = np.array(my_list)
+print(my_array)
