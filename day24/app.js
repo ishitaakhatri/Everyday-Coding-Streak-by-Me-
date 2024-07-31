d3be9826d7e88dcd9e8521b434650e98fa1d8c33
ishitaakhatri <ishita4777@gmail.com>
2024-07-31 22:18:09 +0530

    day25

--- day24/app.js
+++ day24/app.js
@@ -1,25 +1,23 @@
-// // get element by id
+// get element by id
 // let heading = document.getElementById("main_heading")
 // console.log(heading)
-// // get element by class 
+// get element by class 
 // let list_items = document.getElementsByClassName("list_items")
 // console.log(list_items)
-// // get element by tagname 
+// get element by tagname 
 // let list_items_by_tags = document.getElementsByTagName("li")
 // console.log(list_items_by_tags)
-// // query selector 
+// query selector 
 // let containers_of_div = document.querySelector("div")
 // console.log(containers_of_div)
-// // query selector all 
+// query selector all 
 // let all_queries = document.querySelectorAll("div")
 // console.log(all_queries)
-
 let title = document.querySelector("#main_heading")
 title.style.color='pink'
 let listitems = document.querySelectorAll(".list_items")
 for(i in listitems)
     listitems[i].style.color = 'blue'
-
 
 console.log(listitems)
 
---
+++ day25/app_day_2.js
@@ -0,0 +1,16 @@
+
+//creating elements 
+
+const unordered_list = document.querySelector('ul');
+const list_item = document.createElement('li');
+
+//adding the element 
+unordered_list.append(list_item);
+// modifying element 
+let first_list_item = document.querySelector('.list_items')
+console.log(first_list_item.innerText)
+console.log(first_list_item.innerHTML)
+console.log(first_list_item.textContent)
+
+list_item.innerText('X-MAN')
+
